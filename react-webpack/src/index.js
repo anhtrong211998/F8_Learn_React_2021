6b1ae2e76b1678b42f02e26b66fbe8aee6cc7adb
anhtrong211998 <anhtrong211998@gmail.com>
2022-03-08 14:56:18 +0700

    WebPacke Example

---
+++ react-webpack/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; // napm thư viên react
+import ReactDOM from 'react-dom'; // nạp thư viện react-dom
+// Tạo component App
+function App(){
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+// Render component App vào #root element
+ReactDOM.render(<App />,document.getElementById('root'));
